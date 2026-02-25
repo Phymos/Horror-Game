e13c50faa27713b9257d3ea25d751b410bdc531a
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-02-25 21:32:16 +0300

    fixed the interaction script, looks like i didnt initialized both scripts

--- Assets/Scripts/InteractScript.cs
+++ Assets/Scripts/InteractScript.cs
@@ -5,20 +5,14 @@ public class InteractScript : MonoBehaviour
 {
     Camera Cam;
     
-    LayerMask interactableDoorLayer = LayerMask.GetMask("DoorLayer");
-    LayerMask interactableKeyLayer = LayerMask.GetMask("KeyLayer");
-
-    pickUpKey pickUpKeyScript;
-    DoorInteract doorOpenCloseScript;
+    LayerMask interactableDoorLayer;
+    LayerMask interactableKeyLayer;
 
     void Awake()
     {
         Cam = Camera.main;
-    }
-
-    void Update()
-    {
-        
+        interactableDoorLayer = LayerMask.GetMask("DoorLayer");
+        interactableKeyLayer = LayerMask.GetMask("KeyLayer");
     }
 
     public void Interact(InputAction.CallbackContext context)
@@ -27,10 +21,12 @@ public class InteractScript : MonoBehaviour
         {
             if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out RaycastHit hit, 3f, interactableDoorLayer))
             {
+                DoorInteract doorOpenCloseScript = hit.collider.GetComponent<DoorInteract>();
                 doorOpenCloseScript.doorOpenClose();
             }
             else if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out RaycastHit hit2, 3f, interactableKeyLayer))
             {
+                pickUpKey pickUpKeyScript = hit2.collider.GetComponent<pickUpKey>();
                 pickUpKeyScript.keyPickUp();
             }
         }
