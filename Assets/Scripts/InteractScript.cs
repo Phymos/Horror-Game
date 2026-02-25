e8e195e723bf5aba1f3b32718c32dfe7c49e0da7
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-02-25 21:15:19 +0300

    added another script and did some part of it for every interactable object (aka doors and keys)

---
+++ Assets/Scripts/InteractScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractScript : MonoBehaviour
+{
+    Camera Cam;
+    
+    LayerMask interactableDoorLayer = LayerMask.GetMask("DoorLayer");
+    LayerMask interactableKeyLayer = LayerMask.GetMask("KeyLayer");
+
+    pickUpKey pickUpKeyScript;
+
+    void Awake()
+    {
+        Cam = Camera.main;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out RaycastHit hit, 3f, interactableDoorLayer))
+            {
+                doorOpenClose();
+            }
+            else if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out RaycastHit hit2, 3f, interactableKeyLayer))
+            {
+                pickUpKeyScript.keyPickUp();
+            }
+        }
+    }
+}
--- Assets/Scripts/pickUpKey.cs
+++ Assets/Scripts/pickUpKey.cs
@@ -6,37 +6,18 @@ public class pickUpKey : MonoBehaviour
 {
     public static bool isKeyPickedUp = false;
     public Collider keyCollider;
-    bool raycastCheck = false;
 
     public List<GameObject> Monsters;
     public StatueMonsterAi statueMonsterAi;
 
-    Ray ray;
-    RaycastHit hit;
-
-    void Update()
+    public void keyPickUp()
     {
-        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        checkPickUp();
-    }
-
-    void checkPickUp()
-    {
-        if(Physics.Raycast(ray, out hit, 100))
+        isKeyPickedUp = true;
+        Destroy(keyCollider.gameObject);
+        foreach (GameObject monster in Monsters)
         {
-            if(hit.collider == keyCollider)
-            {
-                if (Keyboard.current.eKey.wasPressedThisFrame)
-                {
-                    isKeyPickedUp = true;
-                    Destroy(keyCollider.gameObject);
-                    foreach (GameObject monster in Monsters)
-                    {
-                        statueMonsterAi = monster.GetComponent<StatueMonsterAi>();
-                        statueMonsterAi.enabled = true;
-                    }
-                }
-            }
+            statueMonsterAi = monster.GetComponent<StatueMonsterAi>();
+            statueMonsterAi.enabled = true;
         }
     }
 }
