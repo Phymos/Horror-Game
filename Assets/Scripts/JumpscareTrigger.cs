3c5000409c92d2965b02a63daf93c41f7587e20b
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-03-05 20:35:58 +0300

    doing death scene from zero, some part of it is done

--- Assets/Scripts/JumpscareTrigger.cs
+++ Assets/Scripts/JumpscareTrigger.cs
@@ -1,12 +1,10 @@
-using Unity.Cinemachine;
 using UnityEngine;
 
 public class JumpscareTrigger : MonoBehaviour
 {
-    public CinemachineCamera playerCam;
-    public CinemachineCamera jumpscareCam;
+    public Transform cameraPivot;
     AudioSource audioSource;
-    public AudioClip scaryClip;
+    //public AudioClip scaryClip;
 
     bool triggered = false;
 
@@ -15,14 +13,29 @@ public class JumpscareTrigger : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
+    void Update()
+    {
+        if (triggered)
+        {
+            cameraPivot.localRotation = Quaternion.RotateTowards(cameraPivot.localRotation, Quaternion.Euler(20f, 0f, 85f), 2000f * Time.deltaTime);
+
+            cameraPivot.localRotation *= Quaternion.Euler(0, 0, Random.Range(-1f, 1f));
+
+            //bir fotoğraf olacak siyah, yavaşça siyaha dönecek.
+        }   
+
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (triggered) return;
         if (!other.CompareTag("Player")) return;
 
+        other.GetComponent<PlayerController>().enabled = false;
+        other.GetComponentInChildren<Animator>().enabled = false;
+
         triggered = true;
 
-        audioSource.PlayOneShot(scaryClip);
-        jumpscareCam.Priority = 2;
+        //audioSource.PlayOneShot(scaryClip);
     }
 }
