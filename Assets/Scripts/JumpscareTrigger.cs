d4108d392e58027c3fca9b270678e203c2beba62
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-03-05 22:12:23 +0300

    made screen fade to black after death

--- Assets/Scripts/JumpscareTrigger.cs
+++ Assets/Scripts/JumpscareTrigger.cs
@@ -1,27 +1,47 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class JumpscareTrigger : MonoBehaviour
 {
+    [SerializeField] float cameraSpeed = 2000f;
+    [SerializeField] float cameraShakeIntensity = 1f;
+    [SerializeField] float vignetteSpeed = 1f;
+    [SerializeField] float blackScreenSpeed = 1f;
+
+
     public Transform cameraPivot;
     AudioSource audioSource;
     //public AudioClip scaryClip;
+    
+    public Volume volume;
+    public Image blackScreen;
+    Vignette vignette;
 
     bool triggered = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        volume.profile.TryGet<Vignette>(out vignette);
     }
 
     void Update()
     {
         if (triggered)
         {
-            cameraPivot.localRotation = Quaternion.RotateTowards(cameraPivot.localRotation, Quaternion.Euler(20f, 0f, 85f), 2000f * Time.deltaTime);
+            cameraPivot.localRotation = Quaternion.RotateTowards(cameraPivot.localRotation, Quaternion.Euler(20f, 0f, 85f), cameraSpeed * Time.deltaTime);
 
-            cameraPivot.localRotation *= Quaternion.Euler(0, 0, Random.Range(-1f, 1f));
+            cameraPivot.localRotation *= Quaternion.Euler(0, 0, Random.Range(-cameraShakeIntensity, cameraShakeIntensity));
+
+            //oyuncunun kafası(kamerası) aşağı düşecek
 
             //bir fotoğraf olacak siyah, yavaşça siyaha dönecek.
+            vignette.intensity.value = Mathf.MoveTowards(vignette.intensity.value, 1f, Time.deltaTime * vignetteSpeed);
+            Color c = blackScreen.color;
+            c.a = Mathf.MoveTowards(blackScreen.color.a, 1f, Time.deltaTime * blackScreenSpeed);
+            blackScreen.color = c;
         }   
 
     }
