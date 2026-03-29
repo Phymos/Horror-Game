3a5f75ea018e0b402d0e79be27d76290326ff2d8
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-03-30 00:48:54 +0300

    added a branch from blender, thus changing this script to fit into it

--- Assets/Scripts/NoiseEmitter.cs
+++ Assets/Scripts/NoiseEmitter.cs
@@ -1,15 +1,25 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class NoiseEmitter : MonoBehaviour
 {
     [SerializeField] float intensity = 2f;
+    [SerializeField] Object NoiseEmitterObject;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             NoiseSystem.MakeNoise(transform.position, intensity);
+
+            Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.isKinematic = false;
+            }
+
+            Destroy(GetComponent<Collider>());
         }
     }
 }
