2b0d4517ae1560c47ac2a1e184c478cdb02acfb0
Yavuz Yılmaz <yavuz.yilmaz4554@gmail.com>
2026-04-12 21:15:40 +0300

    added brightness, and saved the values of other settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Volume postProcessingVolume;
+    private ColorAdjustments colorAdjustments;
+
+    void Start()
+    {
+        postProcessingVolume.profile.TryGet(out colorAdjustments);
+
+        float savedBrightness = PlayerPrefs.GetFloat("Brightness", 0f);
+        SetBrightness(savedBrightness);
+
+        float savedVolume = PlayerPrefs.GetFloat("Volume", 0f);
+        SetVolume(savedVolume);
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        colorAdjustments.postExposure.value = brightness;
+        PlayerPrefs.SetFloat("Brightness", brightness);
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+
+        float savedVolume = PlayerPrefs.GetFloat("Volume", 0f);
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 }
